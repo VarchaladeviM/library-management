ff44b6a4a30126b1e76e4b968b1ca6e9eb1ac967
Varchala <abc@gmail.com>
2026-09-17 21:13:31 +0530

    Initial commit

---
+++ src/main/java/com/example/library/LIbraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LIbraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LIbraryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/library/Repository/LibraryRepository.java
@@ -0,0 +1,6 @@
+package com.example.library.Repository;
+
+import com.example.library.Entity.Library;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface LibraryRepository extends JpaRepository<Library, Long> {
+}
